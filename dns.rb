1fdc5d7cbcfead8e83bcb469c294b4022658330b
Jeroen Jacobs <git@jeroenj.be>
2017-06-07 14:11:52 +0200

    Improves some ruby coding styles

--- dns.rb
+++ dns.rb
@@ -12,7 +12,7 @@ class Record
   base_uri 'https://api.digitalocean.com/v2'
   HEADERS = { 'Authorization' => "Bearer #{TOKEN}" }
 
-  def initialize domain, name
+  def initialize(domain, name)
     @domain = domain
     @name = name
   end
@@ -21,25 +21,25 @@ class Record
     self.class.get("/domains/#{domain}/records?per_page=200", headers: HEADERS)['domain_records']
   end
 
-  def update ip
+  def update(ip)
     record = all.detect { |r| r['type'] == 'A' && r['name'] == name }
     if record['data'] == ip
-      log "Record already set to #{ip}"
+      log("Record already set to #{ip}")
     else
       self.class.put("/domains/#{domain}/records/#{record['id']}", query: { data: ip }, headers: HEADERS)
-      log "Record set to #{ip}"
+      log("Record set to #{ip}")
     end
   end
 
-  def log message
+  def log(message)
     puts "[#{Time.now}] (#{name}.#{domain}) #{message}"
   end
 end
 
 class DoDynDNS
-  def self.push_ip domain, name, ip = fetch_ip
+  def self.push_ip(domain, name, ip = fetch_ip)
     record = Record.new(domain, name)
-    record.update ip
+    record.update(ip)
   end
 
   def self.fetch_ip
